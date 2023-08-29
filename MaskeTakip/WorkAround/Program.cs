e200bd963a9b4110e33c8a931a6be5e760b8ed4a
EmrCpr <emrecpr493@gmail.com>
2023-08-29 21:17:54 +0300

    Degiskenler
    Burdaki repo alma amacim bir blogun tamamini yorum satiri yapmak yerine bir kisayolla methoda alip sakladigim icin

---
+++ MaskeTakip/WorkAround/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace WorkAround
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Degiskenler();//Burda ctrl r m yaptik asagidaki blogu secip method yapmis olduk kutuya koyduk diyebiliriz 
+
+            Console.ReadLine();
+        }
+
+        private static void Degiskenler()
+        {
+            string feedback = "Iyi Gunler Dileriz";
+            bool girisYapmisMi = false;
+            double tutar = 100000;
+
+
+            string ad = "Emre";
+            string soyad = "Capar";
+            int dogumyil = 2002;
+            long tcNo = 12345678901;//neden string yapmadik cunku islem yapmiyoruz zaten niye int degil long cunku hanesi fazla
+
+
+            Console.WriteLine(feedback);
+            Console.WriteLine(tutar * 0.18);
+        }
+    }
+
+    public class Vatandas
+    {
+        string ad = "Emre";
+        string soyad = "Capar";
+        int dogumyil = 2002;
+        long tcNo = 12345678901;
+
+    }
+}
